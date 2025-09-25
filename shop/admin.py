5effc11b9edf8de8b4c94a2baaf485c61e7dd744
hearthdesign <hearthxdesign@gmail.com>
2025-09-26 00:09:31 +0200

    Add: create Theme and Print models with admin registration and initial migration

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Theme, Print
 
-# Register your models here.
+@admin.register(Theme)
+class ThemeAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ('name', 'slug')
+
+@admin.register(Print)
+class PrintAdmin(admin.ModelAdmin):
+    list_display = ('title', 'available')
+    list_filter = ('available', 'themes')
+    search_fields = ('title', 'description')
