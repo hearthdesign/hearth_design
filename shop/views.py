c070e3a8882b9fffc806f46a24a8d0b4bebf849d
hearthdesign <hearthxdesign@gmail.com>
2025-09-25 04:40:23 +0200

    Add: views for illustration and photography  galleries

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Print
 
-# Create your views here.
+# Illustration gallery view
+def illustration_gallery(request):
+    prints = Print.objects.filter(type='illustration', is_active=True)
+    return render(request, 'shop/illustration_gallery.html', {'prints': prints})
+
+# photography gallery view
+def photography_gallery(request):
+    prints = Print.objects.filter(type='photo', is_active=True)
+    return render(request, 'shop/photography_gallery.html', {'prints': prints})
