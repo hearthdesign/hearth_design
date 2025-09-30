a2e98965e7f59f760d1a9e4ce196a6d66a146554
hearthdesign <hearthxdesign@gmail.com>
2025-09-30 19:10:50 +0200

    Add: integrate Stripe API keys in settings; enhance modal styles and navbar layout

--- hearth_shop/hearth_shop/settings.py
+++ hearth_shop/hearth_shop/settings.py
@@ -129,3 +129,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STRIPE_SECRET_KEY = config('STRIPE_SECRET_KEY')
+STRIPE_PUBLISHABLE_KEY = config('STRIPE_PUBLISHABLE_KEY')
