df9ce762407385c7e9ee3a3fe08e1c77a00af3c9
hearthdesign <hearthxdesign@gmail.com>
2025-10-10 11:48:51 +0200

    Add Procfile and runtime.txt for Heroku

--- hearth_shop/hearth_shop/settings.py
+++ hearth_shop/hearth_shop/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%b1m8$n*5%)l=q8xo-!!4n7^%5o4#-spydsm1csq7ri5zs0d%g'
 
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
